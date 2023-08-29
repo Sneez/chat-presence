09d17b589801a139b10773830fe6e7147bcc8928
Admin <Admin@MacBook.local>
2023-08-28 18:01:10 -0700

    got basic directive console logging user data on mouseover

--- src/components/ChatroomList.vue
+++ src/components/ChatroomList.vue
@@ -3,13 +3,25 @@ import { ref } from "vue";
 import { allChats } from '../chatrooms';    
 
 const chats = ref(allChats)
+
+const vPresence = {
+    mounted: (el: HTMLElement, binding: any) => {
+        console.log(el)
+        el.addEventListener("mouseover", (event) => {
+            console.log(binding.value)
+
+        });
+
+    }
+}
+
 </script>
 
 <template>
     <div class="section">
         <div class="container">
             <nav class="is-primary panel">
-                <div v-for="chat of chats"
+                <div v-for="chat of chats" v-presence="chat.users" :id="chat.id"
                 class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
                     {{ chat.name }}
                 </div>
