37abaa7c14fc7d729c719e11e3afac60b8fd731f
Admin <Admin@MacBook.lan>
2023-08-28 22:51:44 -0700

    got component with usernames and images to show and hide on mouseover/mouseout

--- src/components/ChatroomList.vue
+++ src/components/ChatroomList.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { allChats } from '../chatrooms';    
+import { ref, createApp } from "vue";
+import { allChats } from '../chatrooms';   
+import PresencePopover from "./PresencePopover.vue"; 
 
 const chats = ref(allChats)
 
 const vPresence = {
     mounted: (el: HTMLElement, binding: any) => {
-        console.log(el)
-        el.addEventListener("mouseover", (event) => {
-            console.log(binding.value)
+        el.addEventListener("mouseover", () => {
+            const div = document.createElement('div');
+            div.id ='presenceDiv'
+            el.appendChild(div);
+            createApp(PresencePopover, {users: binding.value}).mount(div)
 
         });
+        el.addEventListener("mouseout", () => {
+            const divToDelete = document.getElementById('presenceDiv')
+            if(divToDelete !== null){
+                divToDelete.remove()
+            }
+
+        })
 
     }
 }
@@ -23,7 +33,7 @@ const vPresence = {
             <nav class="is-primary panel">
                 <div v-for="chat of chats" v-presence="chat.users" :id="chat.id"
                 class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
-                    {{ chat.name }}
+                    <span>{{ chat.name }}</span>
                 </div>
             </nav>
         </div>
--- src/components/PresencePopover.vue
+++ src/components/PresencePopover.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+import { User } from '../users'
+
+defineProps<{
+    users: Array<User>
+}>()
 
 </script>
 
 <template>
-
-    
+    <div class="section">
+        <div class="container">
+            <div v-for="user of users">
+                <span>{{ user.username }}</span>
+                <img :src="user.imageUrl" class="user-image" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.user-image{
+    max-width: 100px;
+    max-height: 100px;
+    border-radius: 50px;
+}
+</style>
