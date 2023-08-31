6b603b83a97de7c997cffed8eba454e844a1dacc
Admin <Admin@MacBook.lan>
2023-08-30 18:35:25 -0700

    refactored computed properties in popover component

--- src/components/PresencePopover.vue
+++ src/components/PresencePopover.vue
@@ -11,44 +11,34 @@ const props = defineProps<{
 
 const max = props.chatroom.maxPresence
 const userStore = useUsers()
-let currentUserId = computed(() => {
-    if(userStore.currentUserId) {
-        return userStore.currentUserId
-    }
-    else {
-        return ''
-    }
-})
-let leftOutNames = '' 
-let chatUsers = [...props.chatroom.users]
 
 
-function generateUserList( userId?: string ){
-    chatUsers = [...props.chatroom.users]
+let chatUsersExcludingCurrent = computed(() => {
 
+    let chatUsers = [...props.chatroom.users]
     // if current user is in chat users list, remove it
-    const isCurrentUser = (elem: User) => elem.id === userId
+    const isCurrentUser = (elem: User) => elem.id === userStore.currentUserId
     const userIndex = chatUsers.findIndex(isCurrentUser)
     if( userIndex > -1 ){
         chatUsers.splice( userIndex, 1 )
     }
+    return chatUsers
+})
 
+let leftOutNames = computed(() => {
     // create string of usernames of users in the chat not shown (will show on hover of '+x more')
-    leftOutNames = ''
-    if(chatUsers.length > max) {
+    let remainingNamesString = ''
+    if(chatUsersExcludingCurrent.value.length > max) {
         let firstFlag = true
-        chatUsers.slice(max).forEach( user => {
-            firstFlag ? leftOutNames += user.username : leftOutNames += ', ' + user.username
+        chatUsersExcludingCurrent.value.slice(max).forEach( user => {
+            firstFlag ? remainingNamesString += user.username : remainingNamesString += ', ' + user.username
             if (firstFlag) firstFlag = false
         })
     }
-}
 
-generateUserList( currentUserId.value )
-
-watch(currentUserId, (newId) => {
-    generateUserList( newId )
+    return remainingNamesString
 })
+
 </script>
 
 <template>
@@ -57,12 +47,12 @@ watch(currentUserId, (newId) => {
         <div>{{ chatroom.name }}</div>
         <template #content>
             <div class="center-items">
-                <div v-for="user of chatUsers.slice(0, max)" class="bottom-spacing">
+                <div v-for="user of chatUsersExcludingCurrent.slice(0, max)" class="bottom-spacing">
                     <img :src="user.imageUrl" class="user-image" />
                     <div class="user-name">{{ user.username }}</div>
                 </div>
-                <div v-if="chatUsers.length > max" class="user-name" :title="leftOutNames">
-                    +{{ chatUsers.length - max }} more
+                <div v-if="chatUsersExcludingCurrent.length > max" class="user-name" :title="leftOutNames">
+                    +{{ chatUsersExcludingCurrent.length - max }} more
                 </div>
             </div>
         </template>
